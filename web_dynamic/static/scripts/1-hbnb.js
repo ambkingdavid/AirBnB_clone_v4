2dab94ad95c913a12a41609e9bb9225d81bfc456
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-06-22 12:22:31 +0100

    add files

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+   const amenities = {};
+   $('input[type="checkbox"]').change(function () {
+     if ($(this).is(':checked')) {
+       amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+     } else {
+       delete amenities[$(this).attr('data-id')];
+     }
+     const checkedAmenities = Object.values(amenities).join(', ');
+     $('.amenities h4').text(checkedAmenities);
+   });
+ });
